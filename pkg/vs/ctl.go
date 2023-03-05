11340a232469165e610e2178236033b9d5a9bae9
Lain <1297441127@qq.com>
2023-03-05 16:17:30 +0800

    feat: create virtualService

--- pkg/vs/ctl.go
+++ pkg/vs/ctl.go
@@ -3,6 +3,8 @@ package vs
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/xuliangTang/athena/athena"
+	"istio.io/client-go/pkg/apis/networking/v1alpha3"
+	"net/http"
 )
 
 type VsCtl struct {
@@ -19,6 +21,18 @@ func (this *VsCtl) VsList(c *gin.Context) any {
 	return this.VsService.ListByNs(ns)
 }
 
+func (this *VsCtl) CreateVS(ctx *gin.Context) any {
+	vs := &v1alpha3.VirtualService{}
+	athena.Error(ctx.BindJSON(&vs))
+	athena.Error(this.VsService.Create(vs))
+
+	ctx.Set(athena.CtxHttpStatusCode, http.StatusCreated)
+	return vs
+}
+
 func (this *VsCtl) Build(athena *athena.Athena) {
+	// 虚拟服务列表
 	athena.Handle("GET", "/virtualServices", this.VsList)
+	// 创建虚拟服务
+	athena.Handle("POST", "/virtualService", this.CreateVS)
 }
--- pkg/vs/services.go
+++ pkg/vs/services.go
@@ -1,10 +1,16 @@
 package vs
 
-import "istio.io/client-go/pkg/apis/networking/v1alpha3"
+import (
+	"context"
+	"istio.io/client-go/pkg/apis/networking/v1alpha3"
+	"istio.io/client-go/pkg/clientset/versioned"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 // VsService @Service
 type VsService struct {
-	VsMap *VsMapStruct `inject:"-"`
+	VsMap       *VsMapStruct         `inject:"-"`
+	IstioClient *versioned.Clientset `inject:"-"`
 }
 
 func NewVsService() *VsService {
@@ -13,4 +19,10 @@ func NewVsService() *VsService {
 
 func (this *VsService) ListByNs(ns string) []*v1alpha3.VirtualService {
 	return this.VsMap.ListAll(ns)
+}
+
+func (this *VsService) Create(vs *v1alpha3.VirtualService) error {
+	_, err := this.IstioClient.NetworkingV1alpha3().VirtualServices(vs.Namespace).Create(context.Background(), vs, v1.CreateOptions{})
+
+	return err
 }
